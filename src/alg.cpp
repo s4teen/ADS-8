bb18bc5d5669b6ea7ed61863a8954b0aa211f74a
s4teen <112770218+s4teen@users.noreply.github.com>
2026-05-31 19:02:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,68 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <algorithm>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+#include "bst.h"
+
+void makeTree(BST<std::string>& tree, const char* fileName) {
+    std::ifstream file(fileName);
+
+    if (!file.is_open()) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+
+    std::string word;
+    char ch;
+
+    while (file.get(ch)) {
+        if (ch >= 'A' && ch <= 'Z') {
+            word += char(ch - 'A' + 'a');
+        }
+        else if (ch >= 'a' && ch <= 'z') {
+            word += ch;
+        }
+        else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+}
+
+void printFreq(BST<std::string>& tree) {
+    std::vector<std::pair<std::string, int>> words = tree.toVector();
+
+    std::sort(words.begin(), words.end(),
+        [](const std::pair<std::string, int>& a,
+            const std::pair<std::string, int>& b) {
+                if (a.second != b.second) {
+                    return a.second > b.second;
+                }
+                return a.first < b.first;
+        }
+    );
+
+    std::filesystem::create_directories("result");
+
+    std::ofstream out("result/freq.txt");
+
+    if (!out.is_open()) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+
+    for (const auto& item : words) {
+        std::cout << item.first << " " << item.second << std::endl;
+        out << item.first << " " << item.second << std::endl;
+    }
 }
